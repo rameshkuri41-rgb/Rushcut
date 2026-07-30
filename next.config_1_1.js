4eb0536698f5869e18e06f933632cc3ea9009141
rameshkuri41-rgb <rameshkuri41@gmail.com>
2026-07-30 13:10:18 +0530

    Delete next.config_1_1.js

--- next.config_1_1.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverComponentsExternalPackages: ["@prisma/client"] },
-  images: { domains: ["pub-*.r2.dev"] }
-}
-module.exports = nextConfig
